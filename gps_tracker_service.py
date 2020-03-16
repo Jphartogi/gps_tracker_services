5c90bec495851c47d8026d3ffdcb83328a41ebed
Joshua Phartogi <joshuaphartogi@gmail.com>
2020-03-16 09:51:42 +0700

    Fix minor changes

--- gps_tracker_service.py
+++ gps_tracker_service.py
@@ -13,18 +13,21 @@ def getData():
 		r = requests.get(url = yucom_uri,timeout=5)
 	except requests.exceptions.Timeout:
 		print('timeout error')
+		return 0,0,0
 		
 		# Maybe set up for a retry, or continue in a retry loop
 	except requests.exceptions.TooManyRedirects:
 		print('too many redrect timeout')
+		return 0,0,0
 		
 		# Tell the user their URL was bad and try a different one
 	except requests.exceptions.RequestException as e:
 		# catastrophic error. bail.
 		print('errornya :',e)
+		return 0,0,0
 			
      
-  
+	
     # extracting data in json format 
 	data = r.json() 
 	content = data["content"][0]
@@ -63,8 +66,7 @@ def postData(lat,lon,speed):
 
     
 	print("latnya %s longnya %s speednya %s" % (lat,lon,speed))
-
-
+	print(x.text)
 
 def getTime():
   
@@ -72,8 +74,13 @@ def getTime():
     return int(waktu)
 
 if __name__ == "__main__":
-    while True:
-        lat,lon,speed = getData()
-        postData(lat,lon,speed)
-        time.sleep(10)
-    # postData()
+	while True:
+		lat,lon,speed = getData()
+		if lat == 0 and lon == 0 and speed == 0 :
+			print('data failed to retrieve')
+		else:
+			postData(lat,lon,speed)
+			time.sleep(5)
+		time.sleep(1)
+        
+    
